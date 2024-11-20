8eed10ffe2ac8bc5fb666e1f294b5bb8276ff293
Manasvi100 <152421788+Manasvi100@users.noreply.github.com>
2024-11-20 10:07:59 +0530

    Add files via upload

---
+++ CampusConnect.java
@@ -0,0 +1,7 @@
+public class CampusConnect {
+    public static void main(String[] args) {
+        System.out.println("welcome to campus connect");
+        System.out.println("hi arya here");
+        System.out.println("we are working ");
+    }
+}
